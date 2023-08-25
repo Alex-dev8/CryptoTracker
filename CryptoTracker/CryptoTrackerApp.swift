7785bdf01fbeac591c834d87e052bc480efe4dba
Alex Cannizzo <a.cannizzo@apptoyou.it>
2023-08-25 13:46:45 +0700

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Alex Cannizzo on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
