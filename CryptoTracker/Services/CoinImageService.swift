e71a257ac1a5921ad5d269dd594e88d71a27b416
Alex Cannizzo <a.cannizzo@apptoyou.it>
2023-09-03 18:15:28 +0900

    Cache images to File Manager

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -13,14 +13,28 @@ class CoinImageService {
     
     @Published var image: UIImage? = nil
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
     private var imageSubscription: AnyCancellable?
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("[💾] Retrieved image from file manager")
+        } else {
+            downloadCoinImage()
+            print("[⬇️] Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -28,8 +42,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
